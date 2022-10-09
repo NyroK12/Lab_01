4eb29ad5c5c3e628dccdb91f7937c1e4fadc741c
Надя Качкундер <kacgkungerN@outlook.com>
2022-10-09 16:05:58 +0300

    Div

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Git_Lab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Tree");
         }
     }
 }
